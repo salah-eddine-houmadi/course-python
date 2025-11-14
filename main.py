0124acffa09120bb5f926b3b654e7d9defa68212
salah eddine houmadi <79973147+salah-eddine-houmadi@users.noreply.github.com>
2025-11-14 14:38:48 +0100

    introduction python

---
+++ main.py
@@ -0,0 +1,72 @@
+print("hello World")
+print("I am good")
+print("how are yoiu")
+print(3)
+
+age = 4 #integer
+name = "harry" #string
+cgpa = 4.55 #float
+
+age = 3
+print(age)
+
+cgpa = 8.2
+print(type(cgpa))
+
+name = "Harry"
+print(type(name))
+
+is_completed = True 
+print(type(is_completed))
+
+a = 34
+b = "34"
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+a = input("Enter first number")
+a = int(a)
+b = input("Enter second number")
+b = int(b)
+
+print(a+b)
+print ("Helll\nworld!")
+
+a = 34
+
+b = 2
+
+#Arithemtic Operator
+print("a + b",a+b)
+print("a - b",a-b)
+print("a * b",a*b)
+print("a % b",a%b)
+print("a // b",a//b)
+print("a ** b",a**b)
+
+# Conditional Operators
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b) # Is a equal to 4?
+print(a==34) # Is a equal to 34?
+print(a!=b)# Is a not equal to 34?
+
+
+c = True
+d = False
+
+# Logical Operators
+print(c and c)
+print(c and d)
+print(d and c)
+
+print("For or Operator...")
+print(c or c)
+print(c or d)
+print(d or c)
